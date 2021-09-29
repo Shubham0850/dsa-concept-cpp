3870a447766a24dfb4465d64b5e96d7c2d09b338
Shubham0850 <rajshubham0850@gmail.com>
2021-09-29 10:12:20 +0530

    firs commit folder sturcture

---
+++ sorting/counting-sort.cpp
@@ -0,0 +1,14 @@
+/**
+ 
+ Counting short works by itrating through the input, counting the number of times each items occurs, and using those counts to compute an item's index in final sorted array.
+ 
+**/
+
+
+
+/** 
+    Properties of counting short
+    1. Stable
+    2. Time complexity : O(n + k)
+    3. Space complexity : O(n+ k)
+**/ 
